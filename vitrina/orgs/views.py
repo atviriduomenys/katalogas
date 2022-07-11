a2be10323fb71dd8d31b15cf32753118ba4b20e1
Simonas Gliožeris <simonas@tetragrama.com>
2022-07-11 15:33:23 +0300

    add organization title search

--- vitrina/orgs/views.py
+++ vitrina/orgs/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView
+from django.db.models import Q
 
 from vitrina.orgs.models import Organization
 
@@ -8,3 +9,19 @@ class OrganizationListView(ListView):
     queryset = Organization.public.order_by('-created')
     template_name = 'vitrina/orgs/list.html'
     paginate_by = 20
+
+
+class OrganizationSearchResultsView(ListView):
+    model = Organization
+    template_name = 'vitrina/orgs/list.html'
+    paginate_by = 20
+    
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            orgs = Organization.objects.filter(
+                Q(title__icontains=query)
+            )
+        else:
+            orgs = Organization.objects.all()
+        return orgs

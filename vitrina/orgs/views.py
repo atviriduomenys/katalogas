790b40a9c2f2fb8582996100b1d86b53e9ad08c7
adp <adp@atea.lt>
2022-07-13 11:02:29 +0300

    add jurisdiction filter

--- vitrina/orgs/views.py
+++ vitrina/orgs/views.py
@@ -5,6 +5,21 @@ from vitrina.orgs.models import Organization
 
 class OrganizationListView(ListView):
     model = Organization
-    queryset = Organization.public.order_by('-created')
     template_name = 'vitrina/orgs/list.html'
     paginate_by = 20
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        jurisdiction = self.request.GET.get('jurisdiction')
+        orgs = Organization.public.order_by('title')
+
+        if query:
+            orgs = orgs.filter(title__icontains=query)
+        elif jurisdiction:
+            orgs = orgs.filter(jurisdiction=jurisdiction)
+        return orgs
+
+    def get_context_data(self, **kwargs):
+        context = super(OrganizationListView, self).get_context_data(**kwargs)
+        context['jurisdictions'] = Organization.objects.values_list('jurisdiction', flat="True").distinct().order_by('jurisdiction').exclude(jurisdiction__isnull=True)
+        return context

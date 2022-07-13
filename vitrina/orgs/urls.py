a66a2a484cb6bf59273e9d7cf276b4f8dcb95d51
adp-atea <adp@atea.lt>
2022-07-13 15:51:04 +0300

    Added tabs to organization detail view

--- vitrina/orgs/urls.py
+++ vitrina/orgs/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 
 from vitrina.orgs.views import OrganizationListView
-from vitrina.orgs.views import OrganizationDetailView
+from vitrina.orgs.views import OrganizationDetailView, OrganizationRepresentativesView, OrganizationDatasetsView
 
 urlpatterns = [
     # @RequestMapping("/organizations")
     path('organizations/', OrganizationListView.as_view(), name='organization-list'),
-    path('organizations/<kind>/<slug:slug>/', OrganizationDetailView.as_view(), name='organization-detail'),
+    path('orgs/<kind>/<slug:slug>/', OrganizationDetailView.as_view(), name='organization-detail'),
+    path('orgs/<str:kind>/<str:slug>/representatives/', OrganizationRepresentativesView.as_view(),
+         name='organization-representatives'),
+    path('orgs/<str:kind>/<str:slug>/datasets/', OrganizationDatasetsView.as_view(), name='organization-datasets'),
     # @GetMapping("/partner/register")
     # @PostMapping("/partner/register")
     # @GetMapping("/group")
--- vitrina/orgs/views.py
+++ vitrina/orgs/views.py
@@ -10,6 +10,25 @@ class OrganizationListView(ListView):
     template_name = 'vitrina/orgs/list.html'
     paginate_by = 20
 
+
 class OrganizationDetailView(DetailView):
     model = Organization
     template_name = 'vitrina/orgs/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        organization = context_data.get('organization')
+        extra_context_data = {
+            'representatives': organization.representative_set.all(),
+            'datasets': organization.dataset_set.all()
+        }
+        context_data.update(extra_context_data)
+        return context_data
+
+
+class OrganizationRepresentativesView(OrganizationDetailView):
+    template_name = 'vitrina/orgs/representatives.html'
+
+
+class OrganizationDatasetsView(OrganizationDetailView):
+    template_name = 'vitrina/orgs/datasets.html'

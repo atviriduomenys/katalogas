cb6ecd9f8fb0942ad4ecf0855d9d4ff3e2e68298
adp-atea <adp@atea.lt>
2022-08-23 13:16:15 +0300

    update organization filters according to requirements

--- tests/test_organization_filters.py
+++ tests/test_organization_filters.py
@@ -6,15 +6,17 @@ from django_webtest import DjangoTestApp
 from vitrina.orgs.factories import OrganizationFactory
 
 
-@pytest.mark.django_db
-def test_dataset_filter_jurisdiction(app: DjangoTestApp):
-    organization1 = OrganizationFactory(jurisdiction="Jurisdiction1")
-    organization2 = OrganizationFactory(jurisdiction="Jurisdiction2")
-    organization3 = OrganizationFactory(jurisdiction="Jurisdiction2")
+@pytest.fixture
+def organizations():
+    organization1 = OrganizationFactory(title="Organization 1", jurisdiction="Jurisdiction1")
+    organization2 = OrganizationFactory(title="Organization 2", jurisdiction="Jurisdiction2")
+    organization3 = OrganizationFactory(title="Organization 3", jurisdiction="Jurisdiction2")
+    return [organization1, organization2, organization3]
 
-    # without query
+
+@pytest.mark.django_db
+def test_dataset_filter_without_query(app: DjangoTestApp, organizations):
     resp = app.get(reverse('organization-list'))
-    assert resp.status == '200 OK'
     assert len(resp.context['object_list']) == 3
     assert resp.context['selected_jurisdiction'] is None
     assert resp.context['jurisdictions'] == [
@@ -30,11 +32,12 @@ def test_dataset_filter_jurisdiction(app: DjangoTestApp):
         }
     ]
 
-    # with jurisdiction
+
+@pytest.mark.django_db
+def test_dataset_filter_with_jurisdiction(app: DjangoTestApp, organizations):
     resp = app.get("%s?jurisdiction=Jurisdiction1" % reverse('organization-list'))
-    assert resp.status == '200 OK'
     assert len(resp.context['object_list']) == 1
-    assert resp.context['object_list'][0].pk == organization1.pk
+    assert resp.context['object_list'][0].pk == organizations[0].pk
     assert resp.context['selected_jurisdiction'] == "Jurisdiction1"
     assert resp.context['jurisdictions'] == [
         {
@@ -49,12 +52,13 @@ def test_dataset_filter_jurisdiction(app: DjangoTestApp):
         }
     ]
 
-    # with another jurisdiction
+
+@pytest.mark.django_db
+def test_dataset_filter_with_other_jurisdiction(app: DjangoTestApp, organizations):
     resp = app.get("%s?jurisdiction=Jurisdiction2" % reverse('organization-list'))
-    assert resp.status == '200 OK'
     assert len(resp.context['object_list']) == 2
-    assert resp.context['object_list'][0].pk == organization2.pk
-    assert resp.context['object_list'][1].pk == organization3.pk
+    assert resp.context['object_list'][0].pk == organizations[1].pk
+    assert resp.context['object_list'][1].pk == organizations[2].pk
     assert resp.context['selected_jurisdiction'] == "Jurisdiction2"
     assert resp.context['jurisdictions'] == [
         {
@@ -69,9 +73,10 @@ def test_dataset_filter_jurisdiction(app: DjangoTestApp):
         }
     ]
 
-    # with jurisdiction that doesn't exist
+
+@pytest.mark.django_db
+def test_dataset_filter_with_non_existent_jurisdiction(app: DjangoTestApp, organizations):
     resp = app.get("%s?jurisdiction=doesnotexist" % reverse('organization-list'))
-    assert resp.status == '200 OK'
     assert len(resp.context['object_list']) == 0
     assert resp.context['selected_jurisdiction'] == "doesnotexist"
     assert resp.context['jurisdictions'] == [
@@ -83,6 +88,26 @@ def test_dataset_filter_jurisdiction(app: DjangoTestApp):
         {
             'title': 'Jurisdiction2',
             'query': "?jurisdiction=Jurisdiction2",
+            'count': 0
+        }
+    ]
+
+
+@pytest.mark.django_db
+def test_dataset_filter_with_jurisdiction_and_title(app: DjangoTestApp, organizations):
+    resp = app.get("%s?q=1&jurisdiction=Jurisdiction1" % reverse('organization-list'))
+    assert len(resp.context['object_list']) == 1
+    assert resp.context['object_list'][0].pk == organizations[0].pk
+    assert resp.context['selected_jurisdiction'] == "Jurisdiction1"
+    assert resp.context['jurisdictions'] == [
+        {
+            'title': 'Jurisdiction1',
+            'query': "?q=1&jurisdiction=Jurisdiction1",
+            'count': 1
+        },
+        {
+            'title': 'Jurisdiction2',
+            'query': "?q=1&jurisdiction=Jurisdiction2",
             'count': 0
         }
     ]
--- vitrina/orgs/views.py
+++ vitrina/orgs/views.py
@@ -15,18 +15,20 @@ class OrganizationListView(ListView):
 
         if query:
             orgs = orgs.filter(title__icontains=query)
-        elif jurisdiction:
+        if jurisdiction:
             orgs = orgs.filter(jurisdiction=jurisdiction)
         return orgs
 
     def get_context_data(self, **kwargs):
         context = super(OrganizationListView, self).get_context_data(**kwargs)
         filtered_queryset = self.get_queryset()
+        query = self.request.GET.get("q", "")
         context['jurisdictions'] = [{
             'title': jurisdiction,
-            'query': "?jurisdiction=%s" % jurisdiction,
+            'query': "?%s%sjurisdiction=%s" % ("q=%s" % query if query else "", "&" if query else "", jurisdiction),
             'count': filtered_queryset.filter(jurisdiction=jurisdiction).count(),
         } for jurisdiction in Organization.public.values_list('jurisdiction', flat="True")
             .distinct().order_by('jurisdiction').exclude(jurisdiction__isnull=True)]
         context['selected_jurisdiction'] = self.request.GET.get('jurisdiction')
+        context['jurisdiction_query'] = self.request.GET.get("jurisdiction", "")
         return context

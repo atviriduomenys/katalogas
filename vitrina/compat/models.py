716ff8d0bed2457ee86a97cbe778fde1ef7a88be
adp <adp@atea.lt>
2022-08-29 14:08:56 +0300

    fixed kwargs of CharFields

--- vitrina/compat/models.py
+++ vitrina/compat/models.py
@@ -5,8 +5,8 @@ from django.contrib.contenttypes.models import ContentType
 
 class Redirections(models.Model):
     created = models.DateTimeField(blank=True, null=True, auto_now_add=True)
-    name = models.CharField(blank=True, null=True)
-    path = models.CharField(max_length=255, index=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    path = models.CharField(max_length=255, db_index=True)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')

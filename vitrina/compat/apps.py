968c5ef31d812a1b58da86fb1870a4a874e237ae
adp <adp@atea.lt>
2022-08-29 13:52:28 +0300

    Fixed the majority of comments from last review

--- vitrina/compat/apps.py
+++ vitrina/compat/apps.py
@@ -3,6 +3,6 @@ from django.utils.translation import gettext_lazy as _
 
 
 class StructureConfig(AppConfig):
-    name = 'vitrina.redirections'
+    name = 'vitrina.compat'
     label = 'vitrina_redirections'
     verbose_name = _("Redirections")

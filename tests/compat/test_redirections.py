578ad45891b8bf2eef284e054c282d10566a9458
adp <adp@atea.lt>
2022-08-30 10:57:08 +0300

    changed tests to pytest

--- tests/compat/test_redirections.py
+++ tests/compat/test_redirections.py
@@ -1,28 +1,32 @@
 from datetime import datetime
+
+import pytest
 from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase, Client
+from django_webtest import DjangoTestApp
+
 from vitrina.compat.models import Redirections
 from vitrina.datasets.factories import DatasetFactory
 from django.test.utils import override_settings
 
 
-class RedirectionsTest(TestCase):
-    def setUp(self):
-        self.c = Client()
-        self.custom_dataset = DatasetFactory()
-        self.ctype = ContentType.objects.get(model='dataset')
-        Redirections.objects.create(created=datetime.now(),
-                                    name="labas",
-                                    path="/labas/",
-                                    content_type=self.ctype,
-                                    object_id=self.custom_dataset.pk)
+@override_settings(DEBUG=False, ALLOWED_HOSTS=['*'])
+@pytest.mark.django_db
+def test_redirection_doesnt_exist(app: DjangoTestApp):
+    c = Client()
+    response = c.get('/neegzistuoja/')
+    assert response.status_code == 404
 
-    @override_settings(DEBUG=False, ALLOWED_HOSTS=['*'])
-    def test_redirections_doesnt_exist(self):
-        response = self.c.get('/neegzistuoja/')
-        self.assertEqual(response.status_code, 404)
 
-    @override_settings(DEBUG=False, ALLOWED_HOSTS=['*'])
-    def test_redirections_exists(self):
-        response = self.c.get('/labas/')
-        self.assertEqual(response.status_code, 308)
+@override_settings(DEBUG=False, ALLOWED_HOSTS=['*'])
+@pytest.mark.django_db
+def test_redirection_exists(app: DjangoTestApp):
+    custom_dataset = DatasetFactory()
+    ctype = ContentType.objects.get(model='dataset')
+    Redirections.objects.create(created=datetime.now(),
+                                name="labas",
+                                path="/labas/",
+                                content_type=ctype,
+                                object_id=custom_dataset.pk)
+    resp = app.get('/labas/')
+    assert resp.status == '308 Permanent Redirect'

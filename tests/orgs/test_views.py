d34028aee52844b4d4608c890355c10c1b32acd0
adp-atea <adp@atea.lt>
2022-09-02 14:34:13 +0300

    change tests to use WebTest

--- tests/orgs/test_views.py
+++ tests/orgs/test_views.py
@@ -1,45 +1,28 @@
-import pytest
 from django.urls import reverse
-
-from django_webtest import DjangoTestApp
+from django_webtest import WebTest
 
 from vitrina.datasets.factories import DatasetFactory
 from vitrina.orgs.factories import OrganizationFactory, RepresentativeFactory
 
 
-@pytest.fixture
-def data_for_tabs():
-    parent_organization = OrganizationFactory()
-    organization = parent_organization.add_child(**OrganizationFactory.stub().__dict__)
-    dataset = DatasetFactory(organization=organization)
-    representative = RepresentativeFactory(organization=organization)
-    return {
-        'parent': parent_organization,
-        'organization': organization,
-        'dataset': dataset,
-        'representative': representative
-    }
+class OrganizationDetailTest(WebTest):
+    def setUp(self):
+        self.parent_organization = OrganizationFactory()
+        self.organization = self.parent_organization.add_child(**OrganizationFactory.stub().__dict__)
+        self.dataset = DatasetFactory(organization=self.organization)
+        self.representative = RepresentativeFactory(organization=self.organization)
 
+    def test_organization_detail_tab(self):
+        resp = self.app.get(reverse('organization-detail', args=[self.organization.kind, self.organization.slug]))
+        self.assertEqual(list(resp.context['ancestors']), [self.parent_organization])
+        self.assertEqual(list(resp.html.find("li", class_="is-active").a.stripped_strings), ["Informacija"])
 
-@pytest.mark.django_db
-def test_organization_detail_tab(app: DjangoTestApp, data_for_tabs):
-    resp = app.get(reverse('organization-detail', args=[data_for_tabs["organization"].kind,
-                                                        data_for_tabs["organization"].slug]))
-    assert list(resp.context['ancestors']) == [data_for_tabs["parent"]]
-    assert list(resp.html.find("li", class_="is-active").a.stripped_strings) == ["Informacija"]
+    def test_organization_members_tab(self):
+        resp = self.app.get(reverse('organization-members', args=[self.organization.kind, self.organization.slug]))
+        self.assertEqual(list(resp.context['members']), [self.representative])
+        self.assertEqual(list(resp.html.find("li", class_="is-active").a.stripped_strings), ["Organizacijos nariai"])
 
-
-@pytest.mark.django_db
-def test_organization_members_tab(app: DjangoTestApp, data_for_tabs):
-    resp = app.get(reverse('organization-members', args=[data_for_tabs["organization"].kind,
-                                                         data_for_tabs["organization"].slug]))
-    assert list(resp.context['members']) == [data_for_tabs["representative"]]
-    assert list(resp.html.find("li", class_="is-active").a.stripped_strings) == ["Organizacijos nariai"]
-
-
-@pytest.mark.django_db
-def test_organization_dataset_tab(app: DjangoTestApp, data_for_tabs):
-    resp = app.get(reverse('organization-datasets', args=[data_for_tabs["organization"].kind,
-                                                          data_for_tabs["organization"].slug]))
-    assert list(resp.context['object_list']) == [data_for_tabs["dataset"]]
-    assert list(resp.html.find("li", class_="is-active").a.stripped_strings) == ["Duomenų rinkiniai"]
+    def test_organization_dataset_tab(self):
+        resp = self.app.get(reverse('organization-datasets', args=[self.organization.kind, self.organization.slug]))
+        assert list(resp.context['object_list']) == [self.dataset]
+        assert list(resp.html.find("li", class_="is-active").a.stripped_strings) == ["Duomenų rinkiniai"]

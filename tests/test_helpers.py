cf6ee96eff5f58e2c2a814380563e768f435a61d
adp-atea <adp@atea.lt>
2022-09-02 15:43:31 +0300

    change tests to use WebTest

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,62 +1,56 @@
-import pytest
 from django.test import RequestFactory
+from django_webtest import WebTest
 
 from vitrina.helpers import get_selected_value, get_filter_url
 
 
-@pytest.mark.django_db
-def test_get_selected_value(rf: RequestFactory):
-    request = rf.get('/', {'key': 'value'})
-    selected_value = get_selected_value(request, 'key', is_int=False)
-    assert selected_value == 'value'
+class SelectedValueTest(WebTest):
+    def setUp(self):
+        self.rf = RequestFactory()
+
+    def test_get_selected_value(self):
+        request = self.rf.get('/', {'key': 'value'})
+        selected_value = get_selected_value(request, 'key', is_int=False)
+        self.assertEqual(selected_value, 'value')
+
+    def test_get_selected_value_multiple(self):
+        request = self.rf.get('/', {'key': ['value1', 'value2']})
+        selected_value = get_selected_value(request, 'key', multiple=True, is_int=False)
+        self.assertEqual(selected_value, ['value1', 'value2'])
+
+    def test_get_selected_value_int(self):
+        request = self.rf.get('/', {'key': '1'})
+        selected_value = get_selected_value(request, 'key')
+        self.assertEqual(selected_value, 1)
+
+    def test_get_selected_value_multiple_int(self):
+        request = self.rf.get('/', {'key': ['1', '2']})
+        selected_value = get_selected_value(request, 'key', multiple=True)
+        self.assertEqual(selected_value, [1, 2])
+
+    def test_get_selected_value_int_value_error(self):
+        request = self.rf.get('/', {'key': '?'})
+        selected_value = get_selected_value(request, 'key')
+        self.assertIsNone(selected_value)
 
 
-@pytest.mark.django_db
-def test_get_selected_value_multiple(rf: RequestFactory):
-    request = rf.get('/', {'key': ['value1', 'value2']})
-    selected_value = get_selected_value(request, 'key', multiple=True, is_int=False)
-    assert selected_value == ['value1', 'value2']
+class FilterUrlTest(WebTest):
+    def setUp(self):
+        self.rf = RequestFactory()
 
+    def test_get_filter_url(self):
+        request = self.rf.get('/')
+        filter_url = get_filter_url(request, 'key', 'value')
+        self.assertEqual(filter_url, "?key=value")
 
-@pytest.mark.django_db
-def test_get_selected_value_int(rf: RequestFactory):
-    request = rf.get('/', {'key': '1'})
-    selected_value = get_selected_value(request, 'key')
-    assert selected_value == 1
+    def test_get_filter_url_with_append(self):
+        request = self.rf.get('/')
+        filter_url = get_filter_url(request, 'key', 'value', append=True)
+        self.assertEqual(filter_url, "?key=value")
 
-
-@pytest.mark.django_db
-def test_get_selected_value_multiple_int(rf: RequestFactory):
-    request = rf.get('/', {'key': ['1', '2']})
-    selected_value = get_selected_value(request, 'key', multiple=True)
-    assert selected_value == [1, 2]
-
-
-@pytest.mark.django_db
-def test_get_selected_value_int_value_error(rf: RequestFactory):
-    request = rf.get('/', {'key': '?'})
-    selected_value = get_selected_value(request, 'key')
-    assert selected_value is None
-
-
-@pytest.mark.django_db
-def test_get_filter_url(rf: RequestFactory):
-    request = rf.get('/')
-    filter_url = get_filter_url(request, 'key', 'value')
-    assert filter_url == "?key=value"
-
-
-@pytest.mark.django_db
-def test_get_filter_url_with_append(rf: RequestFactory):
-    request = rf.get('/')
-    filter_url = get_filter_url(request, 'key', 'value', append=True)
-    assert filter_url == "?key=value"
-
-
-@pytest.mark.django_db
-def test_get_filter_url_with_append_and_existing_key(rf: RequestFactory):
-    request = rf.get('/', {"key1": "value1"})
-    filter_url = get_filter_url(request, 'key2', 'value2', append=True)
-    assert filter_url == "?key1=value1&key2=value2"
+    def test_get_filter_url_with_append_and_existing_key(self):
+        request = self.rf.get('/', {"key1": "value1"})
+        filter_url = get_filter_url(request, 'key2', 'value2', append=True)
+        self.assertEqual(filter_url, "?key1=value1&key2=value2")
 
 

c2f12052ae9479e4715d4f458fae599b7da26219
adp-atea <adp@atea.lt>
2022-09-06 14:26:03 +0300

    change back tests to pytest style

--- tests/users/test_views.py
+++ tests/users/test_views.py
@@ -1,101 +1,110 @@
+import pytest
 from django.core import mail
 from django.urls import reverse
-from django_webtest import WebTest
+from django_webtest import DjangoTestApp
 
 from vitrina.users.models import User
 
 
 credentials_error = "Neteisingi prisijungimo duomenys"
 name_error = "Vardas negali būti trumpesnis nei 3 simboliai, negali turėti skaičių"
-email_error = "Naudotojas su tokiu el. pašto adresu jau egzistuoja"
 terms_error = "Turite sutikti su naudojimo sąlygomis"
 reset_error = "Naudotojas su tokiu el. pašto adresu neegzistuoja"
 
 
-class UserAuthTest(WebTest):
-    csrf_checks = False
-
-    def setUp(self):
-        self.user = User.objects.create_user(email="test@test.com", password="test123")
-
-
-class LoginTest(UserAuthTest):
-    def test_with_wrong_credentials(self):
-        resp = self.client.post(reverse('login'), {'email': "test@test.com", 'password': "wrongpassword"})
-        self.assertEqual(list(resp.context['form'].errors.values()), [[credentials_error]])
-        self.assertFalse(resp.wsgi_request.user.is_authenticated)
-        self.assertIsNone(resp.wsgi_request.user.pk)
-
-    def test_with_correct_credentials(self):
-        resp = self.client.post(reverse('login'), {'email': "test@test.com", 'password': "test123"})
-        self.assertEqual(resp.status_code, 302)
-        self.assertEqual(resp.url, reverse('home'))
-        self.assertTrue(resp.wsgi_request.user.is_authenticated)
-        self.assertEqual(resp.wsgi_request.user.pk, self.user.pk)
+@pytest.fixture
+def user():
+    user = User.objects.create_user(email="test@test.com", password="test123")
+    return user
 
 
-class RegisterTest(UserAuthTest):
-    def test_with_short_name(self):
-        resp = self.client.post(reverse('register'), {
-            'first_name': "T",
-            'last_name': 'User',
-            'email': "test_@test.com",
-            'password1': "test123?",
-            'password2': "test123?",
-            'agree_to_terms': True
-        })
-        self.assertEqual(list(resp.context['form'].errors.values()), [[name_error]])
-        self.assertEqual(User.objects.filter(email='test_@test.com').count(), 0)
-
-    def test_with_name_with_numbers(self):
-        resp = self.client.post(reverse('register'), {
-            'first_name': "T3st",
-            'last_name': 'User',
-            'email': "test_@test.com",
-            'password1': "test123?",
-            'password2': "test123?",
-            'agree_to_terms': True
-        })
-        self.assertEqual(list(resp.context['form'].errors.values()), [[name_error]])
-        self.assertEqual(User.objects.filter(email='test_@test.com').count(), 0)
-
-    def test_without_agreeing_to_terms(self):
-        resp = self.client.post(reverse('register'), {
-            'first_name': "Test",
-            'last_name': 'User',
-            'email': "test_@test.com",
-            'password1': "test123?",
-            'password2': "test123?",
-            'agree_to_terms': False
-        })
-        self.assertEqual(list(resp.context['form'].errors.values()), [[terms_error]])
-        self.assertEqual(User.objects.filter(email='test_@test.com').count(), 0)
-
-    def test_with_correct_data(self):
-        resp = self.client.post(reverse('register'), {
-            'first_name': "Test",
-            'last_name': 'User',
-            'email': "test_@test.com",
-            'password1': "test123?",
-            'password2': "test123?",
-            'agree_to_terms': True
-        })
-        self.assertEqual(resp.status_code, 302)
-        self.assertEqual(resp.url, reverse('home'))
-        self.assertEqual(User.objects.filter(email='test_@test.com').count(), 1)
-        self.assertTrue(resp.wsgi_request.user.is_authenticated)
-        self.assertEqual(resp.wsgi_request.user.pk, User.objects.filter(email='test_@test.com').first().pk)
+@pytest.mark.django_db
+def test_login_with_wrong_credentials(csrf_exempt_django_app: DjangoTestApp, user: User):
+    resp = csrf_exempt_django_app.post(reverse('login'), {
+        'email': "test@test.com",
+        'password': "wrongpassword"
+    })
+    assert list(resp.context['form'].errors.values()) == [[credentials_error]]
 
 
-class PasswordResetTest(UserAuthTest):
-    def test_with_wrong_email(self):
-        resp = self.client.post(reverse('reset'), {'email': "wrong.email@test.com"})
-        self.assertEqual(list(resp.context['form'].errors.values()), [[reset_error]])
-        self.assertEqual(len(mail.outbox), 0)
+@pytest.mark.django_db
+def test_login_with_correct_credentials(csrf_exempt_django_app: DjangoTestApp, user: User):
+    resp = csrf_exempt_django_app.post(reverse('login'), {
+        'email': "test@test.com",
+        'password': "test123"
+    })
+    assert resp.status_code == 302
+    assert resp.url == reverse('home')
 
-    def test_with_correct_email(self):
-        resp = self.client.post(reverse('reset'), {'email': "test@test.com"})
-        self.assertEqual(resp.status_code, 302)
-        self.assertEqual(resp.url, reverse('home'))
-        self.assertEqual(len(mail.outbox), 1)
-        self.assertEqual(mail.outbox[0].to, [self.user.email])
+
+@pytest.mark.django_db
+def test_register_with_short_name(csrf_exempt_django_app: DjangoTestApp):
+    resp = csrf_exempt_django_app.post(reverse('register'), {
+        'first_name': "T",
+        'last_name': 'User',
+        'email': "test_@test.com",
+        'password1': "test123?",
+        'password2': "test123?",
+        'agree_to_terms': True
+    })
+    assert list(resp.context['form'].errors.values()) == [[name_error]]
+    assert User.objects.filter(email='test_@test.com').count() == 0
+
+
+@pytest.mark.django_db
+def test_register_with_name_with_numbers(csrf_exempt_django_app: DjangoTestApp):
+    resp = csrf_exempt_django_app.post(reverse('register'), {
+        'first_name': "T3st",
+        'last_name': 'User',
+        'email': "test_@test.com",
+        'password1': "test123?",
+        'password2': "test123?",
+        'agree_to_terms': True
+    })
+    assert list(resp.context['form'].errors.values()) == [[name_error]]
+    assert User.objects.filter(email='test_@test.com').count() == 0
+
+
+@pytest.mark.django_db
+def test_register_without_agreeing_to_terms(csrf_exempt_django_app: DjangoTestApp):
+    resp = csrf_exempt_django_app.post(reverse('register'), {
+        'first_name': "Test",
+        'last_name': 'User',
+        'email': "test_@test.com",
+        'password1': "test123?",
+        'password2': "test123?",
+        'agree_to_terms': False
+    })
+    assert list(resp.context['form'].errors.values()) == [[terms_error]]
+    assert User.objects.filter(email='test_@test.com').count() == 0
+
+
+@pytest.mark.django_db
+def test_register_with_correct_data(csrf_exempt_django_app: DjangoTestApp):
+    resp = csrf_exempt_django_app.post(reverse('register'), {
+        'first_name': "Test",
+        'last_name': 'User',
+        'email': "test_@test.com",
+        'password1': "test123?",
+        'password2': "test123?",
+        'agree_to_terms': True
+    })
+    assert resp.status_code == 302
+    assert resp.url == reverse('home')
+    assert User.objects.filter(email='test_@test.com').count() == 1
+
+
+@pytest.mark.django_db
+def test_reset_password_with_wrong_email(csrf_exempt_django_app: DjangoTestApp, user: User):
+    resp = csrf_exempt_django_app.post(reverse('reset'), {'email': "wrong.email@test.com"})
+    assert list(resp.context['form'].errors.values()) == [[reset_error]]
+    assert len(mail.outbox) == 0
+
+
+@pytest.mark.django_db
+def test_reset_password_with_correct_email(csrf_exempt_django_app: DjangoTestApp, user: User):
+    resp = csrf_exempt_django_app.post(reverse('reset'), {'email': "test@test.com"})
+    assert resp.status_code == 302
+    assert resp.url == reverse('home')
+    assert len(mail.outbox) == 1
+    assert mail.outbox[0].to == [user.email]

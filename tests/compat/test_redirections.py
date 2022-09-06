7fcb69267bc3ecfd547a51f890025b96d4bd5056
adp-atea <adp@atea.lt>
2022-09-06 13:55:20 +0300

    change back tests to pytest style

--- tests/compat/test_redirections.py
+++ tests/compat/test_redirections.py
@@ -1,25 +1,30 @@
-from django_webtest import WebTest
+import pytest
+from django_webtest import DjangoTestApp
 from django.contrib.redirects.models import Redirect
 
 
-class RedirectionsTest(WebTest):
-    def test_redirection_doesnt_exist(self):
-        response = self.client.get('/neegzistuoja/')
-        self.assertEqual(response.status_code, 404)
+@pytest.mark.django_db
+def test_redirection_doesnt_exist(app: DjangoTestApp):
+    response = app.get('/neegzistuoja/', expect_errors=True)
+    assert response.status_code == 404
 
-    def test_redirection_exists_has_new_path(self):
-        Redirect.objects.create(
-            site_id=1,
-            old_path='/labas/',
-            new_path='/labas_naujas/',
-        )
-        response = self.client.get('/labas/')
-        self.assertEqual(response.status_code, 301)
 
-    def test_redirection_exists_no_new_path(self):
-        Redirect.objects.create(
-            site_id=1,
-            old_path='/labas/',
-        )
-        response = self.client.get('/labas/')
-        self.assertEqual(response.status_code, 410)
+@pytest.mark.django_db
+def test_redirection_exists_has_new_path(app: DjangoTestApp):
+    Redirect.objects.create(
+        site_id=1,
+        old_path='/labas/',
+        new_path='/labas_naujas/',
+    )
+    response = app.get('/labas/')
+    assert response.status_code == 301
+
+
+@pytest.mark.django_db
+def test_redirection_exists_no_new_path(app: DjangoTestApp):
+    Redirect.objects.create(
+        site_id=1,
+        old_path='/labas/',
+    )
+    response = app.get('/labas/', expect_errors=True)
+    assert response.status_code == 410

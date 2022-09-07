22d343af2d436739c582ef22d6f9387bdbd2d1df
adp <adp@atea.lt>
2022-09-07 09:12:45 +0300

    Added TODO for when other tasks are complete

--- vitrina/datasets/urls.py
+++ vitrina/datasets/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('datasets/<int:pk>/structure/', DatasetStructureView.as_view(), name='dataset-structure'),
     path('datasets/<int:pk>/structure/download/', DatasetStructureDownloadView.as_view(),
          name='dataset-structure-download'),
-    #TODO: change url according to task
+    #TODO: change url according to task and create the add button in template once merging is complete
     path('datasets/<slug:org_slug>/add', DatasetCreateView.as_view(), name='dataset-add'),
     path('datasets/<slug:org_slug>/<slug:dataset_slug>/update', DatasetUpdateView.as_view(), name='dataset-change')
     # @GetMapping("/harvest/object/{id}")

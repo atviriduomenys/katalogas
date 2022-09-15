0926cc112b29e227cc40926303942f843cf6c741
adp <adp@atea.lt>
2022-09-15 14:05:23 +0300

    Made it so PublicDatasetManager would show all datasets that don't have is_public set to False

--- vitrina/datasets/managers.py
+++ vitrina/datasets/managers.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.db.models import Q
 
 
 class PublicDatasetManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset()
+        return super().get_queryset().filter(~Q(is_public=False))

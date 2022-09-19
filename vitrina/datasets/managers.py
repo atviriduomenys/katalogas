f29335be701f2cc3e82e4800bb172846d5d68c92
adp <adp@atea.lt>
2022-09-19 15:03:48 +0300

    Changed public check and added if deleted is not True

--- vitrina/datasets/managers.py
+++ vitrina/datasets/managers.py
@@ -4,4 +4,4 @@ from django.db.models import Q
 
 class PublicDatasetManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().filter(~Q(is_public=False))
+        return super().get_queryset().filter(is_public=True).filter(~Q(deleted=True))

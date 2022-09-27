18b3f1105dcb81f82aa95f4e35fa72c76f43027e
adp <adp@atea.lt>
2022-09-27 10:41:31 +0300

    Deleted null check for is_public

--- vitrina/datasets/managers.py
+++ vitrina/datasets/managers.py
@@ -4,7 +4,7 @@ from django.db import models
 class PublicDatasetManager(models.Manager):
     def get_queryset(self):
         all_datasets = super().get_queryset()
-        public_datasets = all_datasets.filter(is_public__isnull=False).filter(is_public=True)
+        public_datasets = all_datasets.filter(is_public=True)
         not_deleted_public_datasets = public_datasets.filter(deleted__isnull=True, deleted_on__isnull=True)
         datasets_with_orgs = not_deleted_public_datasets.filter(organization_id__isnull=False)
         return datasets_with_orgs

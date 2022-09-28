45ce7e6d7f13adca3f84badb2f85eaf954fe80dd
Mantas <sirexas@gmail.com>
2022-09-28 16:24:56 +0300

    Fix formatting

--- vitrina/datasets/managers.py
+++ vitrina/datasets/managers.py
@@ -2,9 +2,11 @@ from django.db import models
 
 
 class PublicDatasetManager(models.Manager):
+
     def get_queryset(self):
-        all_datasets = super().get_queryset()
-        public_datasets = all_datasets.filter(is_public=True)
-        not_deleted_public_datasets = public_datasets.filter(deleted__isnull=True, deleted_on__isnull=True)
-        datasets_with_orgs = not_deleted_public_datasets.filter(organization_id__isnull=False)
-        return datasets_with_orgs
+        return super().get_queryset().filter(
+            is_public=True,
+            deleted__isnull=True,
+            deleted_on__isnull=True,
+            organization_id__isnull=False,
+        )

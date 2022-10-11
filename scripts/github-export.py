4c4df6733060c2f64c6e3c161a2e9b65ae7e80b6
Gediminas Šimonis <45205912+Gediminas-msc@users.noreply.github.com>
2022-10-11 14:13:10 +0300

    Update github-export.py

--- scripts/github-export.py
+++ scripts/github-export.py
@@ -1,12 +1,8 @@
-"""
-poetry init -n
-poetry add requests
-poetry add pprintpp
-poetry run python
-"""
-
 import json
 import csv
+import sys
+
+from typing import Optional
 
 from typer import Argument
 from typer import run
@@ -14,117 +10,119 @@ from typer import run
 import requests
 from pprintpp import pprint as pp
 
+
 def main(
-        token: str = Argument(..., help='https://github.com/settings/tokens')
+        token: str = Argument(..., help='https://github.com/settings/tokens'),
+        project_id: Optional[str] = Argument(None)
 ):
     session = requests.Session()
     session.headers['Authorization'] = f'Bearer {token}'
 
-    pp(session.post('https://api.github.com/graphql', json={'query': '''\
-    query{
-        organization(login: "atviriduomenys"){
-          projectsNext(first: 10) {
-            nodes {
-              id
-              title
+    if project_id is None:
+        pp(session.post('https://api.github.com/graphql', json={'query': '''\
+        query{
+            organization(login: "atviriduomenys"){
+              projectsNext(first: 10) {
+                nodes {
+                  id
+                  title
+                }
+              }
             }
-          }
         }
-    }
-    '''}).json())
-    # | {'id': 'PN_kwDOAKaBNM4AGx3p', 'title': 'Palaikymas'},
+        '''}).json())
+        exit()
 
-    with open('/tmp/Github_project_to_csv_file.csv', 'w') as f_csv:
-        writer = csv.writer(f_csv)
+    writer = csv.writer(sys.stdout)
 
-        loop = True
-        cursor = None
-        while loop:
-            resp = session.post('https://api.github.com/graphql', json={
-                'query': '''\
-                 query cards($project: ID!, $cursor: String) {
-                  node(id: $project) {
-                    id
-                    ... on ProjectNext {
-                      id
-                      title
-                      items(first: 10, after: $cursor) {
-                        nodes {
-                          content {
-                            ... on Issue {
-                              id
-                              title
-                              number
-                              state
-                              repository {
-                                name
-                              }
-                            }
-                          }
+    loop = True
+    cursor = None
+    while loop:
+        resp = session.post('https://api.github.com/graphql', json={
+            'query': '''\
+             query cards($project: ID!, $cursor: String) {
+              node(id: $project) {
+                id
+                ... on ProjectNext {
+                  id
+                  title
+                  items(first: 10, after: $cursor) {
+                    nodes {
+                      content {
+                        ... on Issue {
+                          id
                           title
-                          fieldValues(first: 10) {
-                            nodes {
-                              value
-                              projectField {
-                                name
-                                settings
-                              }
-                            }
+                          number
+                          state
+                          repository {
+                            name
                           }
                         }
-                        edges {
-                          cursor
-                        }
-                        pageInfo {
-                          hasNextPage
+                      }
+                      title
+                      fieldValues(first: 10) {
+                        nodes {
+                          value
+                          projectField {
+                            name
+                            settings
+                          }
                         }
                       }
                     }
+                    edges {
+                      cursor
+                    }
+                    pageInfo {
+                      hasNextPage
+                    }
                   }
                 }
-                ''',
-                'variables': {
-                    'project': 'PN_kwDOAKaBNM4AGx3p',
-                    'cursor': cursor,
-                }
-            })
+              }
+            }
+            ''',
+            'variables': {
+                'project': project_id,
+                'cursor': cursor,
+            }
+        })
 
-            resp.raise_for_status()
-            project = resp.json()
+        resp.raise_for_status()
+        project = resp.json()
 
-            with open('/tmp/data.json', 'a') as f:
-                json.dump(project, f, indent=2)
+        with open('/tmp/data.json', 'a') as f:
+            json.dump(project, f, indent=2)
 
-            if project['data']['node']['items']['edges']:
-                cursor = project['data']['node']['items']['edges'][-1]['cursor']
+        if project['data']['node']['items']['edges']:
+            cursor = project['data']['node']['items']['edges'][-1]['cursor']
+        else:
+            cursor = None
+        loop = project['data']['node']['items']['pageInfo']['hasNextPage']
+
+        for card in project['data']['node']['items']['nodes']:
+            if card['content']:
+                data = card['content'].copy()
+                data['repository'] = card['content']['repository']['name']
             else:
-                cursor = None
-            loop = project['data']['node']['items']['pageInfo']['hasNextPage']
+                data = {}
 
-            for card in project['data']['node']['items']['nodes']:
-                if card['content']:
-                    data = card['content'].copy()
-                    data['repository'] = card['content']['repository']['name']
-                else:
-                    data = {}
+            for field in card['fieldValues']['nodes']:
+                name = field['projectField']['name']
+                value = field['value']
+                if 'sttings' in field['projectField']:
+                    settings = json.loads(field['projectField']['settings'])
+                    if 'options' in settings:
+                        options = {
+                            opt['id']: opt['name']
+                            for opt in settings['options']
+                        }
+                        if value in options:
+                            value = options[value]
+                data[name] = value
+            pp(data)
 
-                for field in card['fieldValues']['nodes']:
-                    name = field['projectField']['name']
-                    value = field['value']
-                    if 'sttings' in field['projectField']:
-                        settings = json.loads(field['projectField']['settings'])
-                        if 'options' in settings:
-                            options = {
-                                opt['id']: opt['name']
-                                for opt in settings['options']
-                            }
-                            if value in options:
-                                value = options[value]
-                    data[name] = value
-                pp(data)
-
-                for key in data.keys():
-                    writer.writerow([key, data[key]])
+            for key in data.keys():
+                writer.writerow([key, data[key]])
 
 
 if __name__ == '__main__':

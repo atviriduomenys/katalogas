e8541238536b06f934a51369e9ab65c24aa64fae
adp-atea <adp@atea.lt>
2023-01-04 11:56:29 +0200

    fix exception message, add styles to disabled api key in representative form

--- vitrina/api/exceptions.py
+++ vitrina/api/exceptions.py
@@ -7,11 +7,11 @@ class DuplicateAPIKeyException(APIException):
     status_code = status.HTTP_403_FORBIDDEN
     default_detail = _(
         "Baigėsi Jūsų API rakto galiojimas. "
-        "Raktą galite atsinaujinti savo organizacijos tvarkytojų sąraše"
+        "Raktą galite atsinaujinti savo organizacijos tvarkytojų sąraše: {url}"
     )
     default_code = 'duplicate_api_key'
 
     def __init__(self, detail=None, code=None, url=None):
         if url:
-            detail = f"{self.default_detail}: {url}"
+            detail = self.default_detail.format(url=url)
         super().__init__(detail, code)

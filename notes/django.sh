ccb27f1c28bfea85808834ac172a30dd122a3af5
Mantas <sirexas@gmail.com>
2023-03-07 11:43:22 +0200

    Generate new data dump

--- notes/django.sh
+++ notes/django.sh
@@ -12,7 +12,9 @@ poetry run python manage.py collectstatic
 (cd webpack && npm install && npm run build)
 
 # Run dev server
-poetry run python manage.py runserver
+test -n "$PID" && kill $PID
+poetry run python manage.py runserver --noreload &>> var/vitrina.log &; PID=$!
+tail -50 var/vitrina.log
 
 # Anonymize production data
 poetry run python scripts/anonymize.py postgresql://adp:secret@127.0.0.1:5432/adp-dev
@@ -36,3 +38,42 @@ poetry run python manage.py makemessages -av1
 poetry run python manage.py compilemessages
 #| processing file django.po in /home/sirex/dev/data/katalogas/vitrina/locale/en/LC_MESSAGES
 #| processing file django.po in /home/sirex/dev/data/katalogas/vitrina/locale/lt/LC_MESSAGES
+
+
+# 2023-03-07 11:34 Recreate anonymized database dump
+
+# Drop all data in database
+docker-compose run -T --rm -e PGPASSWORD=secret postgres \
+    psql -h postgres -U adp adp-dev <<EOF
+BEGIN TRANSACTION;
+  DROP SCHEMA "public" CASCADE;
+  CREATE SCHEMA "public";
+COMMIT;
+EOF
+
+# Load database dump
+pg_restore -Fc -h localhost -U adp -d adp-dev < var/postgres_adp.dump
+
+psql -h localhost -U adp -d adp-dev <<EOF
+-- Move FK from cms_page to adp_cms_page and drop cms_page,
+-- because cms_page is used by Django CMS.
+ALTER TABLE "cms_attachment"
+    DROP CONSTRAINT "fkb61y6xtsjm3lph7jvqwvna336";
+ALTER TABLE ONLY "cms_attachment"
+    ADD CONSTRAINT "fkb61y6xtsjm3lph7jvqwvna336"
+        FOREIGN KEY (cms_page_id)
+        REFERENCES adp_cms_page(id)
+        ON UPDATE RESTRICT ON DELETE RESTRICT;
+-- Drop duplicate constraint.
+ALTER TABLE "cms_attachment"
+    DROP CONSTRAINT "fkd24jc8oag6itrip826odio0d";
+DROP TABLE "cms_page";
+EOF
+
+poetry run python scripts/anonymize.py postgresql://adp:secret@localhost:5432/adp-dev
+#| You are about to anonymize 10718 rows. Are you sure? [y/N]: y
+y
+#| 100% 10718/10718 [00:29<00:00, 359.50it/s]
+pg_dump -Fc -h localhost -U adp adp-dev > var/postgres_adp_anonymized.dump
+du -sh var/postgres_adp_anonymized.dump
+pg_restore -Fc -h localhost -U adp -d adp-dev < postgres_adp_anonymized.dump

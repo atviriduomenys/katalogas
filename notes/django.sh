9349239ae43534b87ef2a8a0f28ec97033c0079d
Mantas <sirexas@gmail.com>
2023-03-07 12:17:02 +0200

    Update notes

--- notes/django.sh
+++ notes/django.sh
@@ -1,3 +1,6 @@
+# Run all services
+docker-compose up -d
+
 # Install all dependencies
 poetry install
 
@@ -7,19 +10,34 @@ poetry run pytest -vvxra --tb=short --no-migrations tests
 # Run migrations
 poetry run python manage.py migrate
 
+# Run one time migrations on fresh database dump
+poetry run python scripts/migrate_files.py \
+    -distribution-path var/data/ \
+    --cms-path var/data/files/ \
+    --structure-path var/data/structure/
+poetry run python scripts/migrate_pages.py
+poetry run python scripts/migrate_news.py
+
+# Reset passwords of all users
+poetry run python manage.py shell
+from vitrina.users.models import User
+user = User()
+user.set_password('secret')
+User.objects.update(password=user.password)
+exit()
+
+# Update search index
+poetry run python manage.py rebuild_index --noinput --using default
+
 # Static files
-poetry run python manage.py collectstatic
-(cd webpack && npm install && npm run build)
+poetry run python manage.py collectstatic --noinput
+(cd webpack && npm install)
+(cd webpack && npm run build)
 
 # Run dev server
 test -n "$PID" && kill $PID
 poetry run python manage.py runserver --noreload &>> var/vitrina.log &; PID=$!
 tail -50 var/vitrina.log
-
-# Anonymize production data
-poetry run python scripts/anonymize.py postgresql://adp:secret@127.0.0.1:5432/adp-dev
-pg_dump -Fc -h localhost -U adp adp-dev > var/postgres_adp-dev_anonymized.dump
-pg_restore -Fc -h localhost -U adp -d adp-dev < postgres_adp-dev_anonymized.dump
 
 
 # 2022-12-14 09:54 Check translations

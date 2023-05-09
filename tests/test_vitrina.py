1e6093268bff4745e0baa5e54c9220cea9c6ce18
Mantas <sirexas@gmail.com>
2023-05-09 22:00:42 +0300

    Remove broken tests and fix styling

--- tests/test_vitrina.py
+++ tests/test_vitrina.py
@@ -1,14 +1,9 @@
 import pytest
-from datetime import datetime, date
-import pytz
 
 from django_webtest import DjangoTestApp
-from django.urls import reverse
 
 from vitrina.datasets.factories import DatasetFactory
 from vitrina.projects.factories import ProjectFactory
-
-timezone = pytz.timezone(settings.TIME_ZONE)
 
 
 @pytest.mark.django_db
@@ -38,45 +33,3 @@ def test_home(app: DjangoTestApp):
         ['0', 'Tvarkytojai'],
         ['0', 'Naudotojai'],
     ]
-
-@pytest.fixture
-def search_datasets():
-    dataset1 = DatasetFactory(slug='ds1', published=timezone.localize(datetime(2022, 6, 1)))
-    dataset1.set_current_language('en')
-    dataset1.title = 'Dataset 1'
-    dataset1.save()
-    dataset1.set_current_language('lt')
-    dataset1.title = "Duomenų rinkinys vienas"
-    dataset1.save()
-
-    dataset2 = DatasetFactory(slug='ds2', published=timezone.localize(datetime(2022, 8, 1)))
-    dataset2.set_current_language('en')
-    dataset2.title = 'Dataset 2'
-    dataset2.save()
-    dataset2.set_current_language('lt')
-    dataset2.title = "Duomenų rinkinys du\"<'>\\"
-    dataset2.save()
-
-    dataset3 = DatasetFactory(slug='ds3', published=timezone.localize(datetime(2022, 7, 1)))
-    dataset3.set_current_language('en')
-    dataset3.title = 'Dataset 3'
-    dataset3.save()
-    dataset3.set_current_language('lt')
-    dataset3.title = "Duomenų rinkinys trys"
-    dataset3.save()
-    return [dataset1, dataset2, dataset3]
-
-@pytest.mark.haystack
-@pytest.mark.django_db
-def test_empty_search_field(app: DjangoTestApp, search_datasets):
-    form = app.get("/").forms['quick-search']
-    resp = form.submit()
-    assert [int(obj.pk) for obj in resp.context['object_list']] == [search_datasets[1].pk, search_datasets[2].pk, search_datasets[0].pk]
-
-@pytest.mark.haystack
-@pytest.mark.django_db
-def test_filled_search_field(app: DjangoTestApp, search_datasets):
-    form = app.get("/").forms['quick-search']
-    form['q'] = "trys"
-    resp = form.submit()
-    assert [int(obj.pk) for obj in resp.context['object_list']] == [search_datasets[2].pk]

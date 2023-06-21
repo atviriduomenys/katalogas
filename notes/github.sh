f4369e03c918ec4db1f315257ae97b03eb5c94c5
Mantas <sirexas@gmail.com>
2023-06-21 19:47:28 +0300

    An attempt to export github issues

--- notes/github.sh
+++ notes/github.sh
@@ -15,7 +15,7 @@ gh issue list
 gh api graphql --paginate -f query='
 query($endCursor: String) {
   repository(owner: "atviriduomenys", name: "spinta") {
-    issues(last: 1, after: $endCursor) {
+    issues(first: 100, after: $endCursor) {
       pageInfo {
         hasNextPage
         endCursor
@@ -86,4 +86,101 @@ query($endCursor: String) {
     }
   }
 }
-'
+' > var/issues-spinta.json
+
+gh api graphql --paginate -f query='
+query($endCursor: String) {
+  repository(owner: "atviriduomenys", name: "katalogas") {
+    issues(first: 100, after: $endCursor) {
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+      nodes {
+        number
+        state
+        title
+        labels(first: 10) {
+          nodes {
+            name
+            color
+          }
+        }
+        milestone {
+          number
+          state
+          title
+        }
+        projectItems (first: 5) {
+          nodes {
+            project {
+              number
+              title
+            }
+            fieldValues(first: 10) {
+              nodes {
+                ... on ProjectV2ItemFieldIterationValue {
+                  title
+                  startDate
+                  duration
+                  field {
+                    ... on ProjectV2IterationField {
+                      name
+                    }
+                  }
+                }
+                ... on ProjectV2ItemFieldNumberValue {
+                  number
+                  field {
+                    ... on ProjectV2Field {
+                      name
+                    }
+                  }
+                }
+                ... on ProjectV2ItemFieldSingleSelectValue {
+                  name
+                  field {
+                    ... on ProjectV2SingleSelectField {
+                      name
+                    }
+                  }
+                }
+                ... on ProjectV2ItemFieldTextValue {
+                  text
+                  field {
+                    ... on ProjectV2Field {
+                      name
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      totalCount
+    }
+  }
+}
+' > var/issues-katalogas.json
+
+
+cat  var/issues-spinta.json | jq '.data.repository.issues.nodes[0]'
+
+cat var/issues-spinta.json | jq -c '
+.data.repository.issues.nodes[] |
+    select(.projectItems.nodes[] | any(.project.title == "Portalo plėtra")) | [
+    .number,
+    .state,
+    .title,
+    (.projectItems.nodes[] | select(.project.title == "Portalo plėtra") | [
+        .project.title
+    ])
+]
+' | head -n 100
+
+    .projectItems.nodes[] | select(.project.title == "Portalo plėtra") | [
+        .project.title
+    ]
+
+jq var/issues-katalogas.json .

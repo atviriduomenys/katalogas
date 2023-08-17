3da2669f48e5a69e44e939037022bae30d7517c2
Mantas <sirexas@gmail.com>
2023-08-17 08:39:25 +0300

    Testing notes

--- notes/scripts/manifest.sh
+++ notes/scripts/manifest.sh
@@ -10,7 +10,7 @@ docker-compose ps
 docker-compose stop mysql
 
 poetry run python scripts/manifest_export.py --help
-poetry run python scripts/manifest_export.py --manifest-path ../manifest
-#| django.db.utils.ProgrammingError: column organization.name does not exist
-#| LINE 1: ...anization"."image_id", "organization"."provider", "organizat...
+poetry run python scripts/manifest_export.py --manifest-path ../manifest/
+
+poetry run python scripts/manifest_import.py --manifest-path ../manifest/
 

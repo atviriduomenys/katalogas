1a281b4218cd3fa2345ae3e997fef9596d83b6e0
ADP-ATEA <adp@atea.lt>
2023-09-11 13:49:52 +0300

    update condition

--- vitrina/datasets/views.py
+++ vitrina/datasets/views.py
@@ -943,11 +943,6 @@ class DatasetProjectsView(
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['dataset'] = self.object
-        context['can_add_projects'] = has_perm(
-            self.request.user,
-            Action.UPDATE,
-            self.object,
-        )
         context['can_view_members'] = has_perm(
             self.request.user,
             Action.VIEW,

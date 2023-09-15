1bcfb652f90b2b9278e6af2fe08e8f35a229a669
ADP-ATEA <adp@atea.lt>
2023-09-15 13:54:28 +0300

    492 update

--- scripts/downloadstats.py
+++ scripts/downloadstats.py
@@ -11,6 +11,7 @@ import requests as req
 
 from typer import run, Option
 from typer import Argument
+from collections import deque
 
 bots = {
     'SemrushBot',
@@ -28,20 +29,20 @@ bots = {
 
 
 def main(
-    name: str = Argument(help="stats source name, i.e. get.data.gov.lt"),
-    logfile: str = Argument(help=(
+    name: str = Argument(..., help="stats source name, i.e. get.data.gov.lt"),
+    logfile: str = Argument(..., help=(
         "log file to read stats from, i.e. accesslog.json"
     )),
     target: str = Option("http://localhost:8000", help=(
         "target server url"
     )),
-    config_dir: str = Option('~/.local/share/vitrina'),
-    data_dir: str = Option('~/.config/vitrina'),
-    config_file: str = Option(),
+    config_file: str = Option('/.config/vitrina/downloadstats.json'),
+    state_file: str = Option('/.local/share/vitrina/state.json'),
+    bot_status_file: str = Option('/.local/share/vitrina/downloadstats.json'),
 ):
-    config_file = os.path.expanduser('~') + '/.config/vitrina/downloadstats.json'
-    state_file = os.path.expanduser('~') + '/.local/share/vitrina/state.json'
-    bot_status_file = os.path.expanduser('~') + '/.local/share/vitrina/downloadstats.json'
+    config_file = os.path.expanduser('~') + config_file
+    state_file = os.path.expanduser('~') + state_file
+    bot_status_file = os.path.expanduser('~') + bot_status_file
     current_state = {}
 
     bots_found = {'agents': {}}
@@ -56,8 +57,9 @@ def main(
         with open(state_file, 'r') as f:
             current_state = json.load(f)
         state = current_state.get('files').get(logfile)
-        existing_size = state.get('size')
-        existing_offset = state.get('offset')
+        if state is not None:
+            existing_size = state.get('size')
+            existing_offset = state.get('offset')
 
     if not os.path.exists(os.path.dirname(bot_status_file)):
         os.makedirs(os.path.dirname(bot_status_file))
@@ -83,81 +85,87 @@ def main(
         return
 
     endpoint_url = urllib.parse.urljoin(target, 'partner/api/1/downloads')
-    session = req.Sesssion()
+    session = req.Session()
 
+    transactions = {}
+
+    d = deque([], maxlen=1000)
     with open(logfile) as f:
         bytesread = 0
         if existing_offset > 0:
             f.seek(existing_offset)
-        while True:
-            line1 = f.readline()
-            line2 = f.readline()
-            bytesread += len((line1 + line2).encode('utf-8'))
-            if 'txn' in line1 and 'txn' in line2:
-                entry1 = json.loads(line1)
-                entry2 = json.loads(line2)
-                entry = entry1 | entry2
-                txn1 = entry1['txn']
-                txn2 = entry2['txn']
-                if txn1 == txn2:
-                    timestamp = entry['time']
-                    dt = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%f%z')
-                if (
-                    'time' in entry and
-                    'model' in entry and
-                    'objects' in entry
-                ):
+        for line in f:
+            bytesread += len(str.encode(line))
+            d.append(line)
+        for i in d:
+            if 'txn' in i:
+                entry = json.loads(i)
+                txn = entry['txn']
+                timestamp = entry['time']
+                dt = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%f%z')
+                requests = 1
+                if txn not in transactions:
+                    transactions[txn] = {'time': dt}
+                if 'model' in entry:
                     model = entry.get('model')
+                    transactions[txn]['model'] = model
+                if 'objects' in entry:
                     objects = entry.get('objects')
-                    date = dt.date()
-                    hour = dt.hour
-                    agent = ''
-                    requests = 0
+                    transactions[txn]['objects'] = objects
+                if 'format' in entry:
+                    frmt = entry.get('format')
+                    transactions[txn]['format'] = frmt
+                if 'type' in entry:
+                    transactions[txn]['type'] = entry.get('type')
+                    transactions[txn]['requests'] = requests
+                if 'agent' in entry:
+                    transactions[txn]['agent'] = entry.get('agent')
+                if txn in transactions:
+                    if transactions[txn].get('objects') is not None and transactions[txn].get('model') is not None:
+                        model = transactions[txn].get('model')
+                        dt = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%f%z')
+                        date = dt.date()
+                        hour = dt.hour
+                        agent = entry.get('agent', '')
+                        obj = [
+                            {
+                                'date': date,
+                                'hour': hour,
+                                'format': frmt,
+                                'reqs': requests,
+                                'count': objects,
+                                'agent': agent,
+                            }
+                        ]
+                        if len(model) > 0:
+                            if model not in final_stats:
+                                final_stats[model] = obj
+                        if agent in bots:
+                            bots_found['agents'] = bots_found.get('agents').get(agent, 0) + 1
+                        else:
+                            format_string = '%Y-%m-%d %H:%M:%S'
+                            date_string = dt.strftime(format_string)
+                            data = {
+                                "source": name,
+                                "model": model,
+                                "format": frmt,
+                                "time": date_string,
+                                "requests": requests,
+                                "objects": objects
+                            }
+                            transactions.pop(txn)
+                            r = session.post(endpoint_url, data=data)
+                            print(f"Status Code: {r.status_code}, Response: {r.json()}")
 
-                    frmt = entry.get('format', '')
-                    if entry.get('type') == 'request':
-                        requests = 1
-                    agent = entry.get('agent', '')
-                    obj = [
-                        {
-                            'date': date,
-                            'hour': hour,
-                            'format': frmt,
-                            'reqs': requests,
-                            'count': objects,
-                            'agent': agent,
-                        }
-                    ]
-                    if model not in final_stats:
-                        final_stats[model] = obj
-                    if agent in bots:
-                        bots_found['agents'] = bots_found.get('agents').get(agent, 0) + 1
-                    else:
-                        format_string = '%Y-%m-%d %H:%M:%S'
-                        date_string = dt.strftime(format_string)
-                        data = {
-                            "source": name,
-                            "model": model,
-                            "format": frmt,
-                            "time": date_string,
-                            "requests": requests,
-                            "objects": objects
-                        }
-                        r = session.post(endpoint_url, data=data)
-                        print(f"Status Code: {r.status_code}, Response: {r.json()}")
+                with open(bot_status_file, "w+") as bot_file:
+                    bot_file.write(json.dumps(bots_found, indent=4))
 
-            with open(bot_status_file, "w+") as bot_file:
-                bot_file.write(json.dumps(bots_found, indent=4))
-
-            if not line1 or not line2:
-                break
-
-        state_entry = {
-            logfile: {
-                'size': file_size,
-                'offset': bytesread
+            state_entry = {
+                logfile: {
+                    'size': file_size,
+                    'offset': bytesread
+                }
             }
-        }
 
     current_state.get('files', {}).update(state_entry)
 
@@ -168,7 +176,7 @@ def main(
         bot_file.write(json.dumps(bots_found, indent=4))
 
     print(f'Total model entries found: {len(final_stats.keys())}')
-    # print(f'Total transaction entries in file: {len(transactions)}')
+    print(f'Total transaction entries in file: {len(transactions)}')
     print(f'Peak Memory Usage = {resource.getrusage(resource.RUSAGE_SELF).ru_maxrss}')
 
 

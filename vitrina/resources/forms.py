f78400120a882510ee9755b127e3e2e8b2d8bdcd
adp-atea <adp@atea.lt>
2023-12-14 09:41:18 +0200

    876 fix dataset distribution validation

--- vitrina/resources/forms.py
+++ vitrina/resources/forms.py
@@ -144,17 +144,18 @@ class DatasetResourceForm(forms.ModelForm):
             ))
 
         fmt = self.cleaned_data.get('format')
-        if not file and url and fmt.extension not in ['URL', 'API', 'UAPI']:
-            self.add_error('format', _(
-                "Pasirinkite nuorodos formatą."
-            ))
-        elif not url and file:
-            fmt_extension = fmt.extension.upper().strip()
-            file_extension = file.extension.upper().strip()
-            if fmt_extension != file_extension:
+        if fmt:
+            if not file and url and fmt.extension not in ['URL', 'API', 'UAPI']:
                 self.add_error('format', _(
-                    "Formatas nesutampa su įkelto failo formatu."
+                    "Pasirinkite nuorodos formatą."
                 ))
+            elif not url and file:
+                fmt_extension = fmt.extension.upper().strip()
+                file_extension = file.extension.upper().strip()
+                if fmt_extension != file_extension:
+                    self.add_error('format', _(
+                        "Formatas nesutampa su įkelto failo formatu."
+                    ))
 
         return self.cleaned_data
 

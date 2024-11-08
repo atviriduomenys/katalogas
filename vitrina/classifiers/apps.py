cb2ab472dd1412adcd454e4a1bc2731b2b9778e6
Tadas-Stankevicius <tadas.stankevicius@atea.lt>
2024-11-08 13:06:16 +0200

    1017 rename classifiers

--- vitrina/classifiers/apps.py
+++ vitrina/classifiers/apps.py
@@ -5,4 +5,4 @@ from django.utils.translation import gettext_lazy as _
 class ClassifiersConfig(AppConfig):
     name = 'vitrina.classifiers'
     label = 'vitrina_classifiers'
-    verbose_name = _("Classifiers")
+    verbose_name = _("Klasifikatoriai")

e4fa1124513b157d7801272aa746c0a5418aa0ff
Tadas-Stankevicius <tadas.stankevicius@atea.lt>
2024-12-03 09:29:33 +0200

    883 fix rounding

--- vitrina/structure/models.py
+++ vitrina/structure/models.py
@@ -40,7 +40,7 @@ class Prefix(models.Model):
 
 class MetadataManager(models.Manager):
     def average_level(self):
-        return self.exclude(average_level__isnull=True).aggregate(Avg('average_level'))['average_level__avg']
+        return round(self.exclude(average_level__isnull=True).aggregate(Avg('average_level'))['average_level__avg'])
 
 
 class Metadata(models.Model):

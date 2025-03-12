3ce72b1df9b087630daf764a420ab866351c7209
varzgalys <varzgalys.tomas@gmail.com>
2025-03-12 11:51:04 +0200

    1056: fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ echo $DJANGO_SETTINGS_MODULE
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate -v 2 || exit 1
 
-gunicorn "vitrina.wsgi -b 0.0.0.0:8000
+gunicorn vitrina.wsgi -b 0.0.0.0:8000

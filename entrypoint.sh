aecca5a5821e83549e438f6f15e15e5881910bcf
varzgalys <varzgalys.tomas@gmail.com>
2025-03-17 13:57:50 +0200

    1056: update RUN_MODE variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-echo $DJANGO_SETTINGS_MODULE
+echo "DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE"
+echo "RUN_MODE=$RUN_MODE"
 
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate -v 2 || exit 1
 
 
-  if [[ $RUN_MODE == "DEVELOPMENT" ]]; then
-    python3 manage.py runserver 0.0.0.0:8000
-  else
-		gunicorn -b 0.0.0.0:8000 -c /app/conf/gunicorn.conf.aws.py wsgi:application --log-file=-
-  fi
+if [[ $RUN_MODE == "DEVELOPMENT" ]]; then
+  python3 manage.py runserver 0.0.0.0:8000
+else
+  gunicorn -b 0.0.0.0:8000 -c /app/conf/gunicorn.conf.aws.py wsgi:application --log-file=-
+fi

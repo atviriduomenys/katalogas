8b5d42bbf280d7b17a4313eb7c515bf2bf21b110
mindaugasstanionis <mindaugas.stanionis@cornercasetech.com>
2025-03-19 11:44:53 +0200

    1371: changing ApiExample model, adding switch functionality

--- vitrina/api_example/models.py
+++ vitrina/api_example/models.py
@@ -4,7 +4,6 @@ from vitrina.datasets.models import Dataset
 
 
 class ApiExample(models.Model):
-    # path = models.CharField(max_length=255)
     dataset = models.OneToOneField(Dataset, on_delete=models.CASCADE, related_name='examples')
     file_data = models.TextField(help_text="getAll yaml example with list of objects")
 

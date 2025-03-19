989d1dbcd194310d65bfa8e3f6531cab1d16d7e0
mindaugasstanionis <mindaugas.stanionis@cornercasetech.com>
2025-03-19 11:44:53 +0200

    1371: changing ApiExample model, adding switch functionality

--- vitrina/api_example/views.py
+++ vitrina/api_example/views.py
@@ -2,28 +2,45 @@ import yaml
 from django.http import JsonResponse
 from .forms import FileUploadForm
 from .models import ApiExample
+from ..datasets.models import Dataset
+from django.shortcuts import get_object_or_404
+
+def handle_yaml_file(uploaded_file):
+    try:
+        file_content = uploaded_file.read().decode('utf-8')
+        yaml.safe_load(file_content)
+        return file_content, None
+    except yaml.YAMLError as e:
+        return None, f"Klaidingas YAML failas: {str(e)}"
+    except Exception as e:
+        return None, f"Įvyko klaida apdorojant failą: {str(e)}"
 
 
-def file_upload_view(request):
-    if request.method == 'POST' and request.FILES.get('file'):
-        model = request.POST.get('model')
-        form = FileUploadForm(request.POST, request.FILES)
-        if form.is_valid():
-            uploaded_file = request.FILES['file']
-            try:
-                file_content = uploaded_file.read().decode('utf-8')
-                yaml.safe_load(file_content)
-                existing_file = ApiExample.objects.filter(file_data=file_content).first()
-                if existing_file:
-                    return JsonResponse({"error": "Toks YAML failas jau buvo įkeltas anksčiau."}, status=400)
-                file_instance = ApiExample(file_data=file_content, path=model)
-                file_instance.save()
+def is_duplicate(file_content):
+    return ApiExample.objects.filter(file_data=file_content).exists()
 
-                return JsonResponse({"message": "YAML failas įkeltas sėkmingai."})
 
-            except yaml.YAMLError as e:
-                return JsonResponse({"error": f"Klaidingas YAML failas: {str(e)}"}, status=400)
-        else:
-            return JsonResponse({"error": "Neteisingas failas."}, status=400)
+def file_upload_view(request, pk):
+    dataset = get_object_or_404(Dataset, id=pk)
 
-    return JsonResponse({"error": "Nepavyko įkelti failo."}, status=400)
+    if request.method != 'POST' or 'file' not in request.FILES:
+        return JsonResponse({"error": "Nepavyko įkelti failo."}, status=400)
+
+    form = FileUploadForm(request.POST, request.FILES)
+    if not form.is_valid():
+        return JsonResponse({"error": "Neteisingas failas."}, status=400)
+
+    uploaded_file = request.FILES['file']
+    file_content, error_message = handle_yaml_file(uploaded_file)
+
+    if error_message:
+        return JsonResponse({"error": error_message}, status=400)
+
+    if is_duplicate(file_content):
+        return JsonResponse({"error": "Toks YAML failas jau buvo įkeltas anksčiau."}, status=400)
+
+    file_instance = ApiExample(file_data=file_content, dataset=dataset)
+    file_instance.save()
+
+    return JsonResponse({"message": "YAML failas įkeltas sėkmingai."})
+

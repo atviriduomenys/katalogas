0119876af07ea30917e54af2302e1f6bc32dde0b
mindaugasstanionis <mindaugas.stanionis@cornercasetech.com>
2025-03-31 14:33:06 +0300

    1371: adding unit tests

--- tests/api_example/test_views.py
+++ tests/api_example/test_views.py
@@ -1,38 +1,18 @@
-from io import BytesIO
-import yaml
 import pytest
-from vitrina.api_example.models import ApiExample
-from vitrina.api_example.views import handle_yaml_file, is_duplicate
+from django.urls import reverse
+from django_webtest import DjangoTestApp
+
 from vitrina.datasets.factories import DatasetFactory
+from vitrina.users.factories import UserFactory
 
-
-def test_valid_yaml():
-    valid_yaml_content = b"key: value"
-    uploaded_file = BytesIO(valid_yaml_content)
-    content, error = handle_yaml_file(uploaded_file)
-    assert error is None
-    assert content == valid_yaml_content.decode('utf-8')
-
-
-def test_invalid_yaml():
-    invalid_yaml_content = b"key: value\n- list item"
-    uploaded_file = BytesIO(invalid_yaml_content)
-    content, error = handle_yaml_file(uploaded_file)
-    assert content is None
-    assert error.startswith("Klaidingas YAML failas")
-
-
-def test_general_error():
-    uploaded_file = BytesIO(b"")
-    content, error = handle_yaml_file(uploaded_file)
-    assert content is None
-    assert error.startswith("Įvyko klaida apdorojant failą")
 
 @pytest.mark.django_db
-def test_is_duplicate_no_existing_duplicate():
+def test_api_example_yaml_file_import_without_permission(app: DjangoTestApp):
+    user = UserFactory()
     dataset = DatasetFactory()
-    file_content = "key: value"
-    ApiExample.objects.create(file_data=file_content, dataset=dataset.id)
-    result = is_duplicate(file_content)
 
-    assert result is False
+    app.set_user(user)
+    url = reverse("file_upload", args=[dataset.pk])
+    resp = app.get(url, expect_errors=True)
+
+    assert resp.status_code == 403
--- vitrina/api_example/views.py
+++ vitrina/api_example/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import CreateView
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 
 from vitrina.api_example.forms import YamlFileUploadForm
 from vitrina.orgs.services import has_perm, Action
-
+from ..orgs.models import Representative
 from ..structure.views import DatasetStructureMixin
+
 from .models import ApiExample
 
 
@@ -18,6 +21,26 @@ class YamlFileImportView(
     form_class = YamlFileUploadForm
     template_name = "base_form.html"
 
+    def get_context_data(self, **kwargs):
+        return {
+            **super().get_context_data(**kwargs),
+            "current_title": _("Pavyzdinio failo importas"),
+            "parent_links": {
+                reverse("home"): _("Pradžia"),
+                reverse("dataset-list"): _("Duomenų rinkiniai"),
+                reverse("dataset-detail", args=[self.dataset.pk]): self.dataset.title,
+            },
+            "parent_title": self.dataset.title,
+            "parent_url": self.dataset.get_absolute_url(),
+            "tabs": "vitrina/datasets/tabs.html",
+            "can_view_members": has_perm(
+                self.request.user,
+                Action.VIEW,
+                Representative,
+                self.dataset,
+            ),
+        }
+
     def has_permission(self):
         return has_perm(
             self.request.user,
@@ -27,5 +50,6 @@ class YamlFileImportView(
 
     def form_valid(self, form):
         file_data = form.cleaned_data["yaml_file"]
+        self.get_manifest(file_data)
         file_instance = ApiExample(yaml_file=file_data, dataset=self.dataset)
         file_instance.save()

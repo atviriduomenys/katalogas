b4ca9225fd0c84bc11ce865ab852219721bfedcb
Martynas Raila <martynas.raila@cornercasetech.com>
2025-08-11 13:54:37 +0300

    1756 Add information_system_type field to InformationSystemResource (Dataset form) create/update

--- vitrina/datasets/forms.py
+++ vitrina/datasets/forms.py
@@ -465,6 +465,56 @@ class InformationSystemResourceForm(BaseResourceForm):
         )
 
 
+class InformationSystemResourceForm(BaseResourceForm):
+    class Meta:
+        model = Dataset
+        fields = (
+            "title",
+            "description",
+            "is_public",
+            "tags",
+            "catalog",
+            "frequency",
+            "access_rights",
+            "files",
+            "name",
+            "contact",
+            "creator",
+            "publisher",
+            "managed_by_publisher",
+            "landing_page",
+            "information_system_type",
+        )
+
+    def __init__(self, request=None, organization=None, *args, **kwargs):
+        super().__init__(request, organization, *args, **kwargs)
+
+        self.helper.layout = Layout(
+            Field("is_public", placeholder=_("Ar duomenys vieši")),
+            Field("title", placeholder=_("Duomenų rinkinio pavadinimas")),
+            Field("name", placeholder=_("Duomenų rinkinio kodinis pavadinimas")),
+            Field("description", placeholder=_("Detalus duomenų rinkinio aprašas")),
+            Field("files"),
+            Field("tags", placeholder=_("Surašykite aktualius raktinius žodžius")),
+            Field("landing_page"),
+            Field("catalog"),
+            Field("frequency"),
+            Field("access_rights"),
+            Field("contact"),
+            Field("managed_by_publisher"),
+            Field("creator"),
+            Field("publisher"),
+            Field("information_system_type"),
+        )
+
+        self.fields["information_system_type"].queryset = Concept.objects.filter(
+            concept_schemas__uri=Dataset.information_system_type_schema_uri
+        )
+        self.fields["information_system_type"].label_from_instance = lambda obj: str(
+            obj.translated_label
+        )
+
+
 class ResourceForm(BaseResourceForm):
     class Meta:
         model = Dataset

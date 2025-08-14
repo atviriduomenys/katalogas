84711f612738cfd2ffaa4e5d47d0371f1fc73a13
Gytis Bendikas <gytisbendikas@Gytiss-MacBook-Pro-2.local>
2025-08-14 16:44:47 +0300

    refactor: move legislation update logic to model

--- vitrina/classifiers/models.py
+++ vitrina/classifiers/models.py
@@ -268,6 +268,7 @@ class ApplicableLegislation(UUIDBaseModel):
         return self.description or self.url
     def update_description(self) -> str | None:
         if title := fetch_page_title(self.url):
+            if title:
             self.description = title
             self.save(update_fields=["description"])
         return title
--- vitrina/datasets/views.py
+++ vitrina/datasets/views.py
@@ -908,13 +908,7 @@ class DatasetCreateView(
             self.object.save()
 
         if applicable_legislation_urls := form.cleaned_data.get("applicable_legislation"):
-            applicable_legislations = []
-            for url in applicable_legislation_urls:
-                applicable_legislation, created = ApplicableLegislation.objects.get_or_create(url=url)
-                if created:
-                    applicable_legislation.update_description()
-                applicable_legislations.append(applicable_legislation)
-            self.object.applicable_legislation.set(applicable_legislations)
+            self.object.update_applicable_legislation(applicable_legislation_urls)
 
         if applicable_legislation_urls := form.cleaned_data.get("applicable_legislation"):
             self.object.update_applicable_legislation(applicable_legislation_urls)

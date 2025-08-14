6e9a5b7bab5df3ebce477d8575bd0eef3d4300e8
Gytis Bendikas <gytisbendikas@Gytiss-MacBook-Pro-2.local>
2025-08-14 16:53:29 +0300

    move timeout to constant

--- vitrina/services.py
+++ vitrina/services.py
@@ -1,8 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
-def fetch_page_title(url: str, timeout=2.5) -> str | None:
-    #TODO: Use Celery for background jobs
+DEFAULT_FETCH_TIMEOUT = 2
+
+def fetch_page_title(url: str, timeout=DEFAULT_FETCH_TIMEOUT) -> str | None:
+    # TODO: Once Celery is installed, move this to a Celery task to avoid blocking request threads.
     try:
         response = requests.get(url, timeout=timeout, headers={"User-Agent": "Mozilla/5.0"})
         response.raise_for_status()

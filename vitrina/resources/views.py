78bf2d35dae34fcd52c39ece8d18a427dfe89a4b
Gytis Bendikas <gytisbendikas@Gytiss-MacBook-Pro-2.local>
2025-08-20 12:50:16 +0300

    remove translations for distribution history comments

--- vitrina/resources/views.py
+++ vitrina/resources/views.py
@@ -175,7 +175,7 @@ class ResourceCreateView(
             self.dataset.status = Dataset.HAS_DATA
             self.dataset.save()
 
-        set_comment(_("Pridėtas naujas duomenų šaltinis \"%(title)s\".") % {"title": resource.title})
+        set_comment(("Pridėtas naujas duomenų šaltinis \"%(title)s\".") % {"title": resource.lt_title()})
         resource.save()
         return redirect(resource.get_absolute_url())
 
@@ -262,7 +262,7 @@ class ResourceUpdateView(
                 level_given=form.cleaned_data.get("level"),
             )
 
-        set_comment(_("Redaguotas duomenų šaltinis \"%(title)s\".") % {"title": resource.title})
+        set_comment(("Redaguotas duomenų šaltinis \"%(title)s\".") % {"title": resource.lt_title()})
         resource.save()
         return redirect(resource.get_absolute_url())
 
@@ -299,7 +299,7 @@ class ResourceDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView
         with create_revision():
             add_to_revision(resource)
             set_user(request.user)
-            set_comment(_("Ištrintas duomenų šaltinis \"%(title)s\".") % {"title": resource.title})
+            set_comment(("Ištrintas duomenų šaltinis \"%(title)s\".") % {"title": resource.lt_title()})
             resource.delete()
 
             if (

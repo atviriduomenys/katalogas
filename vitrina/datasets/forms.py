6a14ca112c85a3e6c9383f98c40e7eaf47fca2e7
Darius Varnelis <darius.varnelis@cornercasetech.com>
2025-08-22 10:30:44 +0300

    [1762]: improve form validation logic

--- vitrina/datasets/forms.py
+++ vitrina/datasets/forms.py
@@ -493,18 +493,24 @@ class InformationSystemResourceForm(BaseResourceForm):
         self.fields["information_system_importance"].label_from_instance = lambda obj: str(obj.translated_label)
 
     def clean_identifier(self) -> str:
-        agency = get_object_or_404(Agency, code="risr")
-        identifier = self.cleaned_data.get('identifier')
+        identifier = self.cleaned_data.get("identifier")
+        if not identifier:
+            return identifier
 
-        if agency and agency.identifier_validation_type and agency.identifier_validation_options:
-            if agency.identifier_validation_type == Agency.IdentifierValidationType.REGEXP:
-                pattern = agency.identifier_validation_options
-                if not re.fullmatch(pattern, identifier):
-                    raise ValidationError(
-                        _("Žymėjimas turi atitikti šabloną: %(pattern)s"),
-                        params={"pattern": pattern},
-                        code="invalid_format"
-                    )
+        agency = get_object_or_404(Agency, code="risr")
+        if (
+            agency.identifier_validation_type
+            == Agency.IdentifierValidationType.REGEXP
+            and agency.identifier_validation_options
+        ):
+            pattern = agency.identifier_validation_options
+            if not re.fullmatch(pattern, identifier):
+                raise ValidationError(
+                    _("Žymėjimas turi atitikti šabloną: %(pattern)s"),
+                    params={"pattern": pattern},
+                    code="invalid_format",
+                )
+
         return identifier
 
 

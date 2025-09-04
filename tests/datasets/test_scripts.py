e9fa2935ac71c70cb42030cf949a3fa46f7806d8
Gustas Badaitis <gustas.badaitis@cornercasetech.com>
2025-09-04 09:12:02 +0300

    MIssing test

--- tests/datasets/test_scripts.py
+++ tests/datasets/test_scripts.py
@@ -1399,9 +1399,9 @@ def test_geoportal_import__distribution_create_with_not_existing_format(app: Dja
         get_data.side_effect = [get_all_mock, get_conditions_mock, get_one_mock]
         geoportal_import()
 
-
-    assert Dataset.objects.count() == 1
-    dataset = Dataset.objects.first()
+    dataset_objects = Dataset.objects.exclude(id=1)
+    assert dataset_objects.count() == 1
+    dataset = dataset_objects.first()
     assert dataset.datasetdistribution_set.count() == 1
     assert dataset.datasetdistribution_set.first().download_url == "https://example.com/file.csv"
     assert dataset.datasetdistribution_set.first().format is None

eacf9bbb71af75f114b72d32ef43d66bd1d95d03
Gustas Badaitis <gustas.badaitis@cornercasetech.com>
2025-09-05 12:55:12 +0300

    Migration switch

--- vitrina/datasets/migrations/0024_auto_20250826_1009.py
+++ vitrina/datasets/migrations/0024_auto_20250826_1009.py
@@ -7,7 +7,7 @@ import uuid
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('vitrina_datasets', '0024_alter_dataset_access_rights'),
+        ('vitrina_datasets', '0023_dataset_applicable_legislation'),
     ]
 
     operations = [
--- vitrina/datasets/migrations/0025_alter_dataset_access_rights.py
+++ vitrina/datasets/migrations/0025_alter_dataset_access_rights.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('vitrina_datasets', '0023_dataset_applicable_legislation'),
+        ('vitrina_datasets', '0024_auto_20250826_1009'),
     ]
 
     operations = [

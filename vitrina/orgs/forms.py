64e9f079fcfa5a6711dc382f56d683a2b7f54378
Martynas Raila <martynas.raila@cornercasetech.com>
2025-09-15 15:11:05 +0300

    1875: PR review fixes
    Added tests and fixes to PR comments. Also, adds missing translation (not related to this task) and moves one test to other place

--- vitrina/orgs/forms.py
+++ vitrina/orgs/forms.py
@@ -370,7 +370,6 @@ class RepresentativeUpdateForm(ModelForm):
     has_api_access = BooleanField(label=_("Suteikti API prieigą"), required=False)
     regenerate_api_key = BooleanField(label=_("Pergeneruoti raktą"), required=False)
     subscribe = BooleanField(label=_("Prenumeruoti pranešimus"), required=False)
-    can_write = BooleanField(label=_("Leidžiama keisti duomenis"), required=False)
 
     object_model = Organization
 
@@ -451,7 +450,6 @@ class RepresentativeCreateForm(ModelForm):
     )
     has_api_access = BooleanField(label=_("Suteikti API prieigą"), required=False)
     subscribe = BooleanField(label=_("Prenumeruoti pranešimus"), required=False, disabled=True, initial=True)
-    can_write = BooleanField(label=_("Leidžiama keisti duomenis"), required=False)
 
     object_model = Organization
     object_id: int

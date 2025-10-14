940c5551b08b286cc4c7166d65e5dfaa2a8413ad
Gytis Bendikas <gytisbendikas@Gytiss-MacBook-Pro-2.local>
2025-10-14 15:50:00 +0300

    update permissions for admin users

--- vitrina/projects/services.py
+++ vitrina/projects/services.py
@@ -8,12 +8,16 @@ from django.db.models import QuerySet
 
 
 def can_update_project(user: User, project: Project) -> bool:
+    if user.is_staff or user.is_superuser:
+        return True
     if project.organization:
         return is_representative(user, project.organization)
     return has_perm(user, Action.UPDATE, project)
 
 
 def can_view_project(user: User, project: Project) -> bool:
+    if user.is_staff or user.is_superuser:
+        return True
     return can_update_project(user, project) or project.status == Project.APPROVED
 
 
@@ -25,6 +29,9 @@ def get_projects(user: User, organization: Organization = None) -> QuerySet["Pro
 
     if not getattr(user, "is_authenticated", False):
         return base_queryset.filter(status=Project.APPROVED)
+
+    if user.is_staff or user.is_superuser:
+        return base_queryset
 
     ct = ContentType.objects.get_for_model(Organization)
     representative_org_ids = Representative.objects.filter(

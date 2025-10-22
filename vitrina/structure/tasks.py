9820ce45350c79f89115a1a109690a02a491a4e4
mindaugasstanionis <mindaugas.stanionis@cornercasetech.com>
2025-10-22 11:48:44 +0300

    2011: Fixes after PR review, adding task to read spinta logs

--- vitrina/structure/tasks.py
+++ vitrina/structure/tasks.py
@@ -1,3 +1,6 @@
+import contextlib
+import io
+
 from celery import shared_task
 from .models import ManifestValidationEntry
 from spinta.core.context import create_context
@@ -9,13 +12,22 @@ from uuid import UUID
 
 @shared_task
 def validate_manifest_task(manifest_id: UUID) -> None:
-    manifest_instance = ManifestValidationEntry.objects.get(pk=manifest_id)
+    manifest = ManifestValidationEntry.objects.get(pk=manifest_id)
     ctx = SimpleNamespace(obj=create_context("cli"))
-    try:
-        check(ctx, manifests=[str(manifest_instance.dsa_file.path)], mode=Mode("internal"))
-        manifest_instance.validation_status = "valid"
-        manifest_instance.error_message = ""
-    except Exception as error:
-        manifest_instance.validation_status = "invalid"
-        manifest_instance.error_message = str(error)
-    manifest_instance.save()
+
+    buf = io.StringIO()
+    with contextlib.redirect_stdout(buf):
+        try:
+            check(ctx, manifests=[str(manifest.manifest_file.path)], mode=Mode("internal"))
+            manifest.validation_status = "valid"
+            manifest.error_message = ""
+        except Exception as e:
+            manifest.validation_status = "invalid"
+            manifest.error_message = f"Exception: {e}"
+
+    logs = buf.getvalue()
+    if "Context:" in logs or "Total errors" in logs:
+        manifest.validation_status = "invalid"
+        manifest.error_message = logs
+
+    manifest.save()

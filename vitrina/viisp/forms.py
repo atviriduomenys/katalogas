adebf5822f655fea472b12b0eddfaba1a9830bf0
mindaugasstanionis <mindaugas.stanionis@cornercasetech.com>
2025-10-28 18:18:08 +0200

    2028: Adding translations

--- vitrina/viisp/forms.py
+++ vitrina/viisp/forms.py
@@ -15,23 +15,23 @@ PROXY_TYPE_CHOICES = [
 
 
 class FakeViispForm(forms.Form):
-    email = forms.EmailField(label="El. paštas", required=True)
-    lt_company_code = forms.CharField(label="Įmonės kodas (LT)", required=False)
-    proxy_type = forms.ChoiceField(label="JA atstovavimo tipas", choices=PROXY_TYPE_CHOICES, required=False)
+    email = forms.EmailField(label=_("El. paštas"), required=True)
+    lt_company_code = forms.CharField(label=_("Įmonės kodas"), required=False)
+    proxy_type = forms.ChoiceField(label=_("JA atstovavimo tipas"), choices=PROXY_TYPE_CHOICES, required=False)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_id = "fake-viisp-form"
         self.helper.layout = Layout(
-            Field("email", placeholder="El. paštas"),
-            Field("lt_company_code", placeholder="Įmonės kodas (LT)"),
+            Field("email"),
+            Field("lt_company_code"),
             Field("proxy_type"),
-            Submit("submit", "Prisijungti", css_class="button is-primary"),
+            Submit("submit", _("Prisijungti"), css_class="button is-primary"),
         )
 
     def clean_email(self):
         email = self.cleaned_data.get("email")
         if not User.objects.filter(email=email).exists():
-            raise ValidationError(_("Naudotojas su tokiu el. paštu nerastas."))
+            raise ValidationError(_("Naudotojas su tokiu el. paštu neegzistuoja."))
         return email

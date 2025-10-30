31dbdda7eef24223a3589b9e410381e334407ba4
geraldaskastauna <geraldaskastauna@gmail.com>
2025-10-30 14:49:23 +0200

    921 Remove automated status comments.

--- vitrina/comments/models.py
+++ vitrina/comments/models.py
@@ -1,4 +1,3 @@
-from django.apps import apps
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
@@ -124,7 +123,6 @@ class Comment(models.Model):
         return True
 
     def body_text(self):
-        Dataset = apps.get_model("vitrina_datasets", "Dataset")
         if self.type == self.REQUEST and self.rel_content_object:
             url = reverse("request-detail", kwargs={"pk": self.rel_content_object.id})
             body_text = _(
@@ -132,15 +130,6 @@ class Comment(models.Model):
             )
         elif self.type == self.PROJECT and self.rel_content_object:
             body_text = _(f"Šis duomenų rinkinys įtrauktas į {self.rel_content_object.get_title()} projektą.")
-        elif self.type == self.STATUS:
-            if isinstance(self.content_object, Request) and self.status == self.OPENED:
-                body_text = _(f"Statusas pakeistas į {Request.FILTER_STATUSES.get(Request.OPENED)}.")
-            elif isinstance(self.content_object, Dataset) and self.status == self.OPENED:
-                body_text = _(f"Statusas pakeistas į {Dataset.HAS_DATA_TITLE}.")
-            else:
-                body_text = _(f"Statusas pakeistas į {self.get_status_display()}.")
-            if self.body:
-                body_text = f"{body_text}\n{self.body}"
         elif self.type == self.PLAN and self.rel_content_object:
             body_text = mark_safe(
                 f'Įtraukta į planą <a href="{self.rel_content_object.get_absolute_url()}">{self.rel_content_object}</a>'

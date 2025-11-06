3f0590b27aa7919643c5feee3b5cee7e588b950a
Gustas Badaitis <gustas.badaitis@cornercasetech.com>
2025-11-06 23:55:16 +0200

    Datafield change

--- vitrina/structure/models.py
+++ vitrina/structure/models.py
@@ -512,7 +512,7 @@ class Version(models.Model):
     description = models.TextField(_("Aprašymas"), null=True, blank=True)
     deployed = models.DateTimeField(_("Įkėlimo į saugyklą data"), null=True, blank=True)
     status = models.CharField(_("Versijos būsena"), max_length=20, choices=VersionStatus.choices, default=VersionStatus.DRAFT)
-    type = models.CharField(_("Versijos rūšis"), max_length=20, choices=VersionType.choices, null=True, blank=True)
+    version_type = models.CharField(_("Versijos rūšis"), max_length=20, choices=VersionType.choices, null=True, blank=True)
     external_version = models.CharField(_("Versijos numeris"), max_length=50, null=True, blank=True)
     major = models.CharField(_("Major versijos numeris"), max_length=50, null=True, blank=True)
     minor = models.CharField(_("Minor versijos numeris"), max_length=50, null=True, blank=True)

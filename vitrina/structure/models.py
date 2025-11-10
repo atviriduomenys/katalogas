317381e5e7fcc2940ff254e868c88730d2d98a59
Gustas Badaitis <gustas.badaitis@cornercasetech.com>
2025-11-10 11:02:37 +0200

    Ruff

--- vitrina/structure/models.py
+++ vitrina/structure/models.py
@@ -512,7 +512,9 @@ class Version(models.Model):
     description = models.TextField(_("Aprašymas"), null=True, blank=True)
     deployed = models.DateTimeField(_("Įkėlimo į saugyklą data"), null=True, blank=True)
     status = models.CharField(_("Versijos būsena"), max_length=20, choices=VersionStatus.choices, null=True)
-    version_type = models.CharField(_("Versijos rūšis"), max_length=20, choices=VersionType.choices, null=True, blank=True)
+    version_type = models.CharField(
+        _("Versijos rūšis"), max_length=20, choices=VersionType.choices, null=True, blank=True
+    )
     external_version = models.CharField(_("Versijos numeris"), max_length=50, null=True, blank=True)
     major = models.IntegerField(_("Pagrindinis versijos numeris"), null=True, blank=True)
     minor = models.IntegerField(_("Mažasis versijos numeris"), null=True, blank=True)

9da335be0e4dd2eb5734e099884b8261b0406e6d
Gytis Bendikas <gytisbendikas@Gytiss-MacBook-Pro-2.local>
2025-12-08 11:19:17 +0200

    track celery task changes with reversion

--- vitrina/celery.py
+++ vitrina/celery.py
@@ -1,10 +1,20 @@
 import os
 
 from celery import Celery
+import reversion
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vitrina.settings")
 app = Celery("vitrina")
 app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
 
 app.conf.timezone = "UTC"
+
+
+class RevisionedTask(app.Task):
+    def __call__(self, *args, **kwargs):
+        with reversion.create_revision():
+            return super().__call__(*args, **kwargs)
+
+
+app.Task = RevisionedTask
+app.autodiscover_tasks()

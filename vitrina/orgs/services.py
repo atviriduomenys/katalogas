325be9347c847cc454d65a90a08ec14ca613dbc1
mindaugasstanionis <mindaugas.stanionis@cornercasetech.com>
2025-12-11 13:15:36 +0200

    2162: Fixing bug with check manager func

--- vitrina/orgs/services.py
+++ vitrina/orgs/services.py
@@ -472,7 +472,7 @@ def determine_user_role(user: User, resource: Dataset) -> Role:
         return Role.OPEN_DATA_REPRESENTATIVE
     if user.is_gov_organization_manager:
         return Role.MANAGER
-    if user.check_gov_organization_manager(include_information_system=True):
+    if user.check_gov_organization_manager_is_representative(information_system_representative_flag=True):
         return Role.INFORMATION_SYSTEM_MANAGER
     return Role.AUTHENTICATED
 

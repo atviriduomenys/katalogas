172eb06a15c4d9ece54479990d25aeed027f3db0
Gustas Badaitis <gustas.badaitis@cornercasetech.com>
2025-12-12 13:03:40 +0200

    Custom typehints

--- vitrina/structure/__init__.py
+++ vitrina/structure/__init__.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from vitrina.datasets.structure import Enum, Base, Property, Model
+from vitrina.structure.models import ParamItem, Param, EnumItem
+
+
+RELATED_OBJECT_TYPE = Model | Property | Base | EnumItem | Enum | Param | ParamItem
+
 
 class VersionStatus(models.TextChoices):
     DRAFT = "DRAFT", _("Juodraštis")
--- vitrina/structure/views.py
+++ vitrina/structure/views.py
@@ -41,7 +41,7 @@ from vitrina.orgs.services import has_perm, Action
 from vitrina.projects.models import Project
 from vitrina.resources.models import DatasetDistribution
 from vitrina.settings import SPINTA_SERVER_URL
-from vitrina.structure import spyna
+from vitrina.structure import spyna, RELATED_OBJECT_TYPE
 from vitrina.structure.forms import (
     EnumForm,
     ModelCreateForm,
@@ -3544,7 +3544,7 @@ class PublishVersionView(PermissionRequiredMixin, CreateView):
 
         return old_metadata_instance, new_metadata_instance
 
-    def duplicate_foreign_key_relationships(self, new_related_object, already_created_fields: dict) -> dict:
+    def duplicate_foreign_key_relationships(self, new_related_object: RELATED_OBJECT_TYPE, already_created_fields: dict) -> dict:
         needed_foreign_key_relationships = [
             Base,
             Model,
@@ -3602,17 +3602,17 @@ class PublishVersionView(PermissionRequiredMixin, CreateView):
 
         return already_created_fields
 
-    def check_if_field_has_same_dataset(self, field) -> bool:
+    def check_if_field_has_same_dataset(self, field: RELATED_OBJECT_TYPE) -> bool:
         return field.metadata_version.dataset == self.dataset
 
-    def check_if_field_has_same_version(self, field) -> bool:
+    def check_if_field_has_same_version(self, field: RELATED_OBJECT_TYPE) -> bool:
         return field.metadata_version == self.metadata_version
 
-    def check_if_field_has_published_version(self, field) -> bool:
+    def check_if_field_has_published_version(self, field: RELATED_OBJECT_TYPE) -> bool:
         return field.metadata_version.status and field.metadata_version.status != VersionStatus.DRAFT
 
     def check_if_field_is_valid(
-        self, deeper_old_related_object, new_related_object, already_created_fields: dict
+        self, deeper_old_related_object: RELATED_OBJECT_TYPE, new_related_object: RELATED_OBJECT_TYPE, already_created_fields: dict
     ) -> None:
         always_valid_fields = [Param, Enum]
         if isinstance(deeper_old_related_object, tuple(always_valid_fields)):

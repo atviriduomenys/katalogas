a5921bce0d66ac7ba7ffa1bdb88f55fb2bd09cd9
Darius Varnelis <darius.varnelis@cornercasetech.com>
2026-03-30 10:25:59 +0300

    [1428]: check agreement link content

--- tests/smart_contracts/test_views.py
+++ tests/smart_contracts/test_views.py
@@ -562,6 +562,10 @@ class TestAgreementSubmit:
 
         assert len(mail.outbox) == 1
         assert mail.outbox[0].to == ["assigner@example.com"]
+        expected_link = "http://testserver" + reverse(
+            "project-agreement-detail", args=[project.pk, agreement.pk]
+        )
+        assert expected_link in mail.outbox[0].body
 
     def test_no_email_sent_when_assigner_has_no_email(self, app: DjangoTestApp, dataset: Dataset):
         assignee_organization = OrganizationFactory(email="assignee@example.com")

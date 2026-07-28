05a1e5c495892d3a6893a0a3e5f09ce4cba637ab
Jonas Prievelis <prievelis.jonas@gmail.com>
2026-07-28 11:31:12 +0300

    fix queryset in test

--- tests/requests/test_forms.py
+++ tests/requests/test_forms.py
@@ -2,8 +2,8 @@ import pytest
 from django.utils.html import escape
 
 from vitrina.plans.factories import PlanFactory
+from vitrina.plans.models import Plan
 from vitrina.requests.forms import PlanChoiceField
-from vitrina.requests.models import Request
 
 XSS_PAYLOAD = "<script>alert('xss')</script>"
 
@@ -11,10 +11,10 @@ XSS_PAYLOAD = "<script>alert('xss')</script>"
 @pytest.mark.django_db
 class TestPlanChoiceFieldRequestsXSS:
     def test_label_from_instance_escapes_title(self):
-        request = PlanFactory(title=XSS_PAYLOAD)
-        field = PlanChoiceField(queryset=Request.objects.all())
+        plan = PlanFactory(title=XSS_PAYLOAD)
+        field = PlanChoiceField(queryset=Plan.objects.all())
 
-        result = field.label_from_instance(request)
+        result = field.label_from_instance(plan)
 
         assert XSS_PAYLOAD not in str(result)
         assert escape(XSS_PAYLOAD) in str(result)

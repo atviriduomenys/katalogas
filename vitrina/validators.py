ef91f7500237da5a9559b9199d1ea5d63c0c4d29
kastger <geraldaskastauna@gmail.com>
2026-08-05 12:02:22 +0300

    2723 add `validate_absolute_uri` and `URIFormField`

--- vitrina/validators.py
+++ vitrina/validators.py
@@ -1,7 +1,8 @@
+from urllib.parse import urlsplit
+
 from django.core.exceptions import ValidationError
-from django.core.validators import RegexValidator
+from django.core.validators import RegexValidator, URLValidator
 from django.utils.translation import gettext_lazy as _
-from vitrina.users.models import OldPassword
 from django.contrib.auth.hashers import check_password
 from zxcvbn import zxcvbn
 import re
@@ -10,6 +11,23 @@ phone_validator = RegexValidator(
     regex=r"^\+370\d{8}$|^0\d{8}$",
     message=_("Neteisingas telefono numerio formatas. Priimtini formatai: +370XXXXXXXX, 0XXXXXXXX"),
 )
+
+ABSOLUTE_URI_SCHEMES = ("http", "https", "ftp", "ftps")
+
+
+def validate_absolute_uri(value: str) -> None:
+    text = str(value)
+    if any(character.isspace() or not character.isprintable() for character in text):
+        raise ValidationError(URLValidator.message, code="invalid")
+
+    try:
+        parts = urlsplit(text)
+        parts.port
+    except ValueError:
+        raise ValidationError(URLValidator.message, code="invalid")
+
+    if parts.scheme.lower() not in ABSOLUTE_URI_SCHEMES or not parts.hostname:
+        raise ValidationError(URLValidator.message, code="invalid")
 
 
 class UppercaseValidator:
@@ -66,6 +84,9 @@ class SpecialCharacterValidator:
 
 class UniquePasswordValidator:
     def validate(self, password, user=None):
+        # Imported here so model modules can import this module without an import cycle.
+        from vitrina.users.models import OldPassword
+
         if user:
             old_passwords = OldPassword.objects.filter(user=user).order_by("-created")[:4]
             for old_password in old_passwords:
